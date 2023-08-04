569bc983185c4af4f1c67d021ea5615daa5a4333
david <cognet.david@hotmail.fr>
2023-08-04 05:37:32 +0200

    ceration component menu

--- my-kasa-app/src/App.js
+++ my-kasa-app/src/App.js
@@ -1,8 +1,14 @@
+import Menu from "./components/menu.js";
 import Home from "./pages/home.js";
 import "./styles/app.css";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Menu />
+      <Home />;
+    </>
+  );
 }
 
 export default App;
---
+++ my-kasa-app/src/components/menu.js
@@ -0,0 +1,13 @@
+export default function Menu() {
+  return (
+    <div className="menu">
+      <picture>
+        <img src="./images/logo" alt="Logo de Kasa"></img>
+      </picture>
+      <ul>
+        <li>Accueil</li>
+        <li>A Propos</li>
+      </ul>
+    </div>
+  );
+}
