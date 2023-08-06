dd6f5de1efddd868fdb663b3fb4a47a2ebd7db92
david <cognet.david@hotmail.fr>
2023-08-06 11:01:53 +0200

    add routes to App.js

--- my-kasa-app/src/App.js
+++ my-kasa-app/src/App.js
@@ -1,5 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import AccomodationInfo from "./pages/AccomodationInfo.js";
+import ErrorPage from "./pages/ErrorPage.js";
 import "./styles/app.scss";
 
 function App() {
@@ -7,6 +10,9 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/accomodationinfo" element={<AccomodationInfo />} />
+        <Route path="/error" element={<ErrorPage />} />
       </Routes>
     </>
   );
