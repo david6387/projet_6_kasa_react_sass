8bd3375825252545ffe71e1f05a363ab271440d3
david <cognet.david@hotmail.fr>
2023-08-06 09:35:27 +0200

    import BrowserRouter depuis index.js

--- my-kasa-app/src/App.js
+++ my-kasa-app/src/App.js
@@ -1,14 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.js";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/app.scss";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
