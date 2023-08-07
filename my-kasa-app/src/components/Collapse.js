69c484b177b7325ca29747e9ffb100b49e5bc7f6
david <cognet.david@hotmail.fr>
2023-08-07 19:00:18 +0200

    test init collapse

--- my-kasa-app/src/components/Collapse.js
+++ my-kasa-app/src/components/Collapse.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function Collapse() {
   return <div className="collapse"></div>;
 }
--- my-kasa-app/src/pages/About.js
+++ my-kasa-app/src/pages/About.js
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
+import Collapse from "../components/Collapse";
 
 export default function About() {
   return (
     <>
       <Banner />
+      <Collapse />
       <div className="about"></div>
     </>
   );
