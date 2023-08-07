71e636c6dd635ca110450b9e4f71d072f9f76433
david <cognet.david@hotmail.fr>
2023-08-07 19:32:38 +0200

    test errorpage

--- my-kasa-app/src/pages/ErrorPage.js
+++ my-kasa-app/src/pages/ErrorPage.js
@@ -1,7 +1,22 @@
+import { NavLink } from "react-router-dom";
+
 export default function ErrorPage() {
   return (
     <>
-      <div className="error-page"></div>
+      <div className="error-page">
+        <div>404</div>
+        <div className="middle">
+          Oups! La page que vous demandez n'existe pas.
+        </div>
+        <div>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+          >
+            Retourner sur la page d’accueil
+          </NavLink>
+        </div>
+      </div>
     </>
   );
 }
