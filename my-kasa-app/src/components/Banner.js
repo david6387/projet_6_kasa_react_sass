a6c51c9c3e550c189df7475460c4c2a5b476215d
david <cognet.david@hotmail.fr>
2023-08-08 17:32:12 +0200

    test overlay banner

--- my-kasa-app/src/components/Banner.js
+++ my-kasa-app/src/components/Banner.js
@@ -1,7 +1,9 @@
 export default function Banner() {
   return (
     <div className="banner">
-      <h1>Chez vous, partout et ailleurs</h1>
+      <div className="overlay">
+        <h1>Chez vous, partout et ailleurs</h1>
+      </div>
     </div>
   );
 }
