dd5833be82304106f58e2ad7d58d1df0bb6ebb06
david <cognet.david@hotmail.fr>
2023-08-10 08:56:42 +0200

    // comment

--- my-kasa-app/src/pages/Home.js
+++ my-kasa-app/src/pages/Home.js
@@ -3,9 +3,9 @@ import Banner from "../components/Banner";
 import Cards from "../components/Cards";
 
 export default function Home() {
-  // les states et les variables
+  // les states et les variables (états, données)
 
-  // les fonctions
+  // les fonctions , les comportements
 
   // render = rendu
   return (
