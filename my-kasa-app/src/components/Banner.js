505df364f886afb6e55f289c430d1b31322f104f
david <cognet.david@hotmail.fr>
2023-08-11 18:23:17 +0200

    style banner

--- my-kasa-app/src/components/Banner.js
+++ my-kasa-app/src/components/Banner.js
@@ -3,10 +3,8 @@ import React from "react";
 export default function Banner({ img, title }) {
   return (
     <div className="banner">
-      <div className="overlay">
-        <img src={img} alt="" />
-        {!title ? "" : <h1>{title}</h1>}
-      </div>
+      <img src={img} alt="" />
+      <div className="overlay">{!title ? "" : <h1>{title}</h1>}</div>
     </div>
   );
 }
