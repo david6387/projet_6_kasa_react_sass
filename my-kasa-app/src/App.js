9d430ce03ac163823af1b56588a3f8e8e69b74d1
david <cognet.david@hotmail.fr>
2023-08-14 20:02:16 +0200

    props for logements

--- my-kasa-app/src/App.js
+++ my-kasa-app/src/App.js
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
@@ -10,11 +10,25 @@ import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
+  // les states et les variables (états, données)
+  const [logements, setLogements] = useState([]);
+
+  // les fonctions , les comportements
+  useEffect(() => {
+    fetch("./data/logements.json")
+      .then((response) => response.json())
+      .then((datas) => {
+        console.log(datas);
+        setLogements(datas);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home logements={logements} />} />
         <Route path="/about" element={<About />} />
         <Route path="/accomodation">
           <Route path=":pageId" element={<AccomodationInfo />} />
--- my-kasa-app/src/pages/Home.jsx
+++ my-kasa-app/src/pages/Home.jsx
@@ -5,20 +5,8 @@ import BannerHome from "../images/mer.png";
 import Card from "../components/Card";
 import { Link } from "react-router-dom";
 
-export default function Home() {
-  // les states et les variables (états, données)
-  const [logements, setLogements] = useState([]);
+export default function Home({logements}) {
 
-  // les fonctions , les comportements
-  useEffect(() => {
-    fetch("./data/logements.json")
-      .then((response) => response.json())
-      .then((datas) => {
-        console.log(datas);
-        setLogements(datas);
-      })
-      .catch((error) => console.log(error));
-  }, []);
 
   // render = rendu
   return (
