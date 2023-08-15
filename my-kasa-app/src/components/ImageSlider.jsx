242a22b4c27564857475e85911500d24fc37ab91
david <cognet.david@hotmail.fr>
2023-08-15 21:16:32 +0200

    test imageSlider

--- my-kasa-app/src/components/ImageSlider.jsx
+++ my-kasa-app/src/components/ImageSlider.jsx
@@ -2,5 +2,11 @@ import React from "react";
 
 export default function ImageSlider({slides}) {
   return (
+    <>
   <img src={slides} alt="coco"/>
+  <div className="overlay">
+    <img src="https://david6387.github.io/projet_6_kasa_react_sass/my-kasa-app/src/images/arrow-back.png" alt="arrow previous"/>
+    <img src="https://david6387.github.io/projet_6_kasa_react_sass/my-kasa-app/src/images/arrow-forward.png" alt="arrow next"/>
+  </div>
+  </>
 )}
