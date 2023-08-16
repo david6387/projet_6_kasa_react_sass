cbcc27fab34cc56419536226bd275f6ee7928861
david <cognet.david@hotmail.fr>
2023-08-16 08:53:33 +0200

    delete uselees component cards & cleanig scss

--- my-kasa-app/src/pages/Home.jsx
+++ my-kasa-app/src/pages/Home.jsx
@@ -1,17 +1,13 @@
-// import React, { useEffect, useState } from "react";
 import Banner from "../components/Banner";
-// import Cards from "../components/Cards";
 import BannerHome from "../images/mer.png";
 import Card from "../components/Card";
 import { Link } from "react-router-dom";
 
 export default function Home({logements}) {
 
-  // render = rendu
   return (
     <>
       <Banner img={BannerHome} title="Chez vous, partout et ailleurs" />
-      {/* <Cards> */}
       <div className="cards">
         {logements &&
           logements.map((logement) => (
@@ -20,7 +16,6 @@ export default function Home({logements}) {
             </Link>
           ))}
       </div>
-      {/* </Cards> */}
     </>
   );
 }
